66a5e9775572e6338b5d575dc299c26daaa6c4c8
andrii <empl.dmytrenko@gmail.com>
2018-03-13 01:06:31 +0200

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 # from django.conf.urls import url
 from . import views
+# from django.urls import include, re_path
 
 urlpatterns = [
     # url(r'^$', views.post_list, name='post_list'),
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    # re_path(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
